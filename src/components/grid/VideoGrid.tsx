5e075e1bcaf3f1196af7dd3d7a97962213efd78d
Apurbo Kumar Anup <apurboka@gmail.com>
2023-08-11 23:52:37 +0600

    Update VideoGrid.tsx

--- src/components/grid/VideoGrid.tsx
+++ src/components/grid/VideoGrid.tsx
@@ -8,12 +8,12 @@ const VideoGrid = () => {
       <section className="pt-12">
         <section className="pt-12">
           <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
+            <VideoGridItem/>
+
+             {/* error section
+            <div className="col-span-12">some error happened</div> */}
            
-           <VideoGridItem/>
-           
-            {/* error section */}
-            <div className="col-span-12">some error happened</div>
-          </div>
+            </div>
         </section>
       </section>
     </>
