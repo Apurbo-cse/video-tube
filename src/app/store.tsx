b2220dfd2af7aaa64e2c006d6f1c853ebf7f1139
Apurbo Kumar Anup <apurboka@gmail.com>
2023-08-12 01:50:57 +0600

    Update store.tsx

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import videoSliceReducr from '../features/videos/videoSlice';
 
-
 export const store = configureStore({
   reducer: {
     counter: videoSliceReducr,
