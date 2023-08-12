beae5b34c395af9964f92c8bbaf17dfef596edc5
Apurbo Kumar Anup <apurboka@gmail.com>
2023-08-12 11:49:51 +0600

    Update VideoDec.tsx

--- src/components/despcription/VideoDec.tsx
+++ src/components/despcription/VideoDec.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import LikeUnLike from "./LikeUnLike";
+interface Video {
+  video: any;
+}
 
-const VideoDec: React.FC = () => {
+const VideoDec: React.FC<Video> = ({video}) => {
+  const  { title, description, date,} = video;
   return (
     <>
       <div>
         <h1 className="text-lg font-semibold tracking-tight text-slate-800">
-          Some video title
+         {title}
         </h1>
         <div className="pb-4 flex items-center space-between border-b">
           <h2 className="text-sm leading-[1.7142857] text-slate-600 w-full">
-            Uploaded on 23 Nov 2022
+            Uploaded on {date}
           </h2>
 
           <LikeUnLike/>
         </div>
 
         <div className="mt-4 text-sm text-[#334155] dark:text-slate-400">
-          Some video description here
+          {description}
         </div>
       </div>
     </>
