07b5622f2b5492284ed8bd4cafc69c9be7dfa72c
Apurbo Kumar Anup <apurboka@gmail.com>
2023-08-12 22:47:57 +0600

    Update Search.tsx

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { searched } from '../../features/filter/filterSlice';
 
-const Search = () => {
+const Search: React.FC = () => {
+
+    const dispatch = useDispatch();
+    const {search} = useSelector((state: any) => state.filter);
+
+    const [input, setInput] = useState<any>(search);
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        dispatch(searched(input))
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     className="outline-none border-none mr-2"
                     type="search"
                     name="search"
                     placeholder="Search"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
                 />
             </form>
         </>
