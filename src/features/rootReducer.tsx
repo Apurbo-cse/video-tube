3e3ec52f4ed7593544ebc353c0fae2d3db595e31
Apurbo Kumar Anup <apurboka@gmail.com>
2023-08-12 10:36:30 +0600

    Update rootReducer.tsx

--- src/features/rootReducer.tsx
+++ src/features/rootReducer.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import videoReducer  from '../features/videos/videoSlice';
+import videoReducer  from './videos/videoSlice';
+import tagReducer from './tags/tagSlice';
 
 const rootReducer = combineReducers({
   videos: videoReducer,
+  tags: tagReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
