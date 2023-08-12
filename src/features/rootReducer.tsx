db611427fc189f6afb9ace30599931827c51730b
Apurbo Kumar Anup <apurboka@gmail.com>
2023-08-12 21:54:32 +0600

    Update rootReducer.tsx

--- src/features/rootReducer.tsx
+++ src/features/rootReducer.tsx
@@ -3,12 +3,14 @@ import videosReducer  from './videos/videoSlice';
 import tagReducer from './tags/tagSlice';
 import videoReducer from './videos/videoDetailSlice';
 import relatedReducer from './videos/relatedVideoSlice';
+import filterReducer from './filter/filterSlice';
 
 const rootReducer = combineReducers({
   videos: videosReducer,
   tags: tagReducer,
   videoDetails: videoReducer,
-  relatedVideo: relatedReducer 
+  relatedVideo: relatedReducer,
+  filter: filterReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
