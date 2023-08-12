331cdd25027ddce53e9c2790e778aa274ad80f4b
Apurbo Kumar Anup <apurboka@gmail.com>
2023-08-12 11:17:20 +0600

    Update videosApi.tsx

--- src/api/videosApi.tsx
+++ src/api/videosApi.tsx
@@ -4,3 +4,8 @@ export const getVideos = async () => {
     const response = await axios.get('/videos');
     return response.data;
 }
+
+export const getVideoDetails = async (id: string) => {
+    const response = await axios.get(`/videos/${id}`);
+    return response.data;
+}
