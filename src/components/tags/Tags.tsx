2a3d86a0805caa753539d2c83000e39026892ab5
Apurbo Kumar Anup <apurboka@gmail.com>
2023-08-12 10:35:08 +0600

    Update Tags.tsx

--- src/components/tags/Tags.tsx
+++ src/components/tags/Tags.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Tag from "./Tag";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTags } from "../../features/tags/tagSlice";
 
-const Tags = () => {
+
+const Tags: React.FC = () => {
+
+  const {} = useSelector((sate) => sate.tags)
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(fetchTags() as any);
+  }, [dispatch]);
+  
+
   return (
-    <>
-      {/* <!-- Tags --> */}
+
       <section>
         <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
          <Tag/>
         </div>
       </section>
-    </>
   );
 };
 
