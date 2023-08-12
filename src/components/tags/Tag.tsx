48df8506f7daad41b92b7781a7ee9d6a2740dc2a
Apurbo Kumar Anup <apurboka@gmail.com>
2023-08-12 22:03:30 +0600

    Update Tag.tsx

--- src/components/tags/Tag.tsx
+++ src/components/tags/Tag.tsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { tagReamoved, tagSelected } from "../../features/filter/filterSlice";
 interface Tag {
   tag: any;
 }
 
 const Tag: React.FC<Tag> = ({tag}) => {
+
+  const dispatch = useDispatch();
+  const {tags : selectedTags} = useSelector( (state: any) => state.filter)
   
+  const isSelected = selectedTags.includes(tag.title) ? true : false;
+  const style = isSelected ? 'bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer' : 'bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer'
+  
+  const handleClick = () => {
+    if(isSelected){
+      dispatch(tagReamoved(tag.title));
+    }else{
+      dispatch(tagSelected(tag.title));
+    }
+  }
+
   return (
-      <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
+      <div className={style} onClick={handleClick}>
         {tag.title}
       </div>
   );
